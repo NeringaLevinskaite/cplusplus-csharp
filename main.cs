e2f612cabae46d139817a69cc812e760ce9448e9
NeringaLevinskaite <neringa.levinskaite@gmail.com>
2015-09-25 12:45:03 +0300

    Nuskaitymas
    Nuskaitymas ir perdavimas eilučių..
    Nieko ypatinga, tik griaučiai

--- main.cs
+++ main.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Bandymas_versti
 {
@@ -10,7 +11,14 @@ namespace Bandymas_versti
     {
         static void Main(string[] args)
         {
+            string nuoroda=@"Z:\KTU\Vertėjas\bandymas.txt";
+            string[] lines = File.ReadAllLines(nuoroda);
+            foreach (string line in lines)
+            {
+                Print print = new Print(line);
+                print.write();
+            }
+            Console.ReadKey(); 
         }
     }
 }
-
