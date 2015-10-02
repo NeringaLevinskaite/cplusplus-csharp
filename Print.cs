e804ca172d35cc59c9c5123315405174784ed94b
NeringaLevinskaite <neringa.levinskaite@gmail.com>
2015-10-02 12:44:10 +0300

    Update Print.cs
    Think about how to push i to back when its the end of line
    The first letter in word disappears

--- Print.cs
+++ Print.cs
@@ -10,15 +10,61 @@ namespace Bandymas_versti
     class Print
     {
         private string line;
-        char[] delimiterChars = { ' ', '<'};
-        //patobulinti
-        public void write()
+        public Print(string Line)
         {
-            string[] words = line.Split(delimiterChars);
-            foreach (string word in words)
+            line = Line;
+        }
+        List<string> words = new List<string>();
+        private void skaidyk()
+        {
+            int n = line.Length;
+            Console.WriteLine("n lygu {0}", n);
+            for (int i = 0; i < n; i++)
             {
-                Console.WriteLine(word);
+                //Console.WriteLine(i);
+                string word="";
+                switch (line[i])
+                {
+                    case ' ':
+                        Console.WriteLine("SPACE");
+                        break;
+                    case '<':
+                        Console.WriteLine("<");
+                        break;
+                    case '"':
+                        for (int j = 1; j < n - i; j++)
+                        {
+                            if (line[i + j] == '"')
+                            {
+                                i += j;
+                                break;
+                            }
+                            word += line[i + j];
+                        }
+                        break;
+                    default:
+                        for (int j = 1; j < n - i; j++)
+                        {
+                            if ((line[i + j] == ' ') || (line[i + j] == '<'))
+                            {
+                                i += j;
+                                break;
+                            }
+                            word += line[i + j];
+                        }
+                        
+                        break;
+                }
+                Console.WriteLine("wordd {0}", word);
+                if (word != "") words.Add(word);
             }
+        }
+        public List<string> demenys()
+        {
+            skaidyk();
+            Console.WriteLine(line.Length);
+            return words;
         }
     }
 }
+
